455f7369d48e96bb82b19d2676dc8670ebfec3f1
JATO PRIESTLY NJILAH NJEP NJEPNYU <123805245+Priestly-flutter@users.noreply.github.com>
2023-11-05 22:01:13 +0100

    Update quiz.js

--- QuizApp/quizes/static/quizes/quiz.js
+++ QuizApp/quizes/static/quizes/quiz.js
@@ -1,12 +1,24 @@
 console.log('hello Priestly I work')
 const url = window.location.href
 
+const quizBox = document.getElementById('quiz-box')
+
+let data
+
 $.ajax({
     type: 'GET',
     url: '${url}data',
 
     success:function(response){
         console.log(response)
+          data = response.data
+        //grabbing element from data base list in a lsit format containing both key and values
+        data.forEach(el => {
+            for(const [question,answers] of Object.entries(el)){
+                console.log(question)
+                console.log(answers)
+            }
+        });
         
     },
     error:function(error){
