9ba7c300f3b49f1be90b0c1e5ac4e024d0b1ba6e
JATO PRIESTLY NJILAH NJEP NJEPNYU <123805245+Priestly-flutter@users.noreply.github.com>
2023-11-05 22:27:14 +0100

    Update quiz.js

--- QuizApp/quizes/static/quizes/quiz.js
+++ QuizApp/quizes/static/quizes/quiz.js
@@ -15,9 +15,21 @@ $.ajax({
         //grabbing element from data base list in a lsit format containing both key and values
         data.forEach(el => {
             for(const [question,answers] of Object.entries(el)){
-                console.log(question)
-                console.log(answers)
-            }
+                quizBox.innerHTML +='
+                        <div class="mb-2">
+                    //injectiing the question in to the page.
+                            <b>${question}</b>
+                        </div>
+                    '
+                     answers.forEach(answer=>{
+                        quizBox.innerHTML +='
+                            <div>
+                                <input type="radio" class="ans" id="${question}-$asnwer}" name="${question} value="${answer}">
+                            <lable for="${question}">${answer}
+                            </div>
+                        '
+                    })
+                }
         });
         
     },
